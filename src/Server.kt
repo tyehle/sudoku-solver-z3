cdb03ccb0a79395179bd60ac6daf83e8d6b95e36
Tobin Yehle <tobinyehle@gmail.com>
2016-01-23 16:37:53 -0700

    added querystring parsing

--- src/Server.kt
+++ src/Server.kt
@@ -17,8 +17,16 @@ fun main(args: Array<String>) {
 }
 
 fun handler3x3(exchange: HttpExchange) {
-    println("Building a 3x3 board")
-    val board = "3 3\n" + puzzleString(randomPuzzle(3, 3))
+    println("Query: ${exchange.requestURI.query}")
+
+    val qs = exchange.requestURI.query.split('&').toMap { kv -> val parts = kv.split('='); Pair(parts[0], parts[1]) }
+
+    fun getInt(key: String): Int = try { qs[key]?.toInt() } catch(e: NumberFormatException) { null } ?: 3
+    val m = getInt("m")
+    val n = getInt("n")
+
+    println("Building a ${m}x$n board")
+    val board = "$m $n\n" + puzzleString(randomPuzzle(m, n))
     println("\nserving:\n$board")
     val response = board.toByteArray()
     exchange.sendResponseHeaders(200, response.size.toLong())
