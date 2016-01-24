5a66b34df34ad3750ca0fd172509f3df7dc91eba
Tobin Yehle <tobinyehle@gmail.com>
2016-01-24 00:40:28 -0700

    update function names, and fix small bug with the qs being empty

--- src/Server.kt
+++ src/Server.kt
@@ -10,19 +10,20 @@ fun puzzleString(board: Board<Int?>): String =
         board.map { if(it == null) "_" else it.toString() }.toString()
 
 fun main(args: Array<String>) {
-    val server = HttpServer.create(InetSocketAddress(6965), 0)
-    server.createContext("/3x3-board.txt", ::handler3x3)
+    val server = HttpServer.create(InetSocketAddress(8080), 0)
+    server.createContext("/board.txt", ::boardHandler)
     server.executor = null
     server.start()
 }
 
-fun handler3x3(exchange: HttpExchange) {
-    println("Handler called")
-    println("Query: ${exchange.requestURI.query}")
+fun boardHandler(exchange: HttpExchange) {
+    val query = exchange.requestURI?.query ?: ""
+    println("Query: $query")
 
-    val qs = exchange.requestURI.query.split('&').toMap { kv -> val parts = kv.split('='); Pair(parts[0], parts[1]) }
+    val qs = query.split('&').filter { it.length > 0 }.toMap { kv -> val parts = kv.split('='); Pair(parts[0], parts[1]) }
 
-    fun getInt(key: String): Int = try { qs[key]?.toInt() } catch(e: NumberFormatException) { null } ?: 3
+    // default to m = n = 2
+    fun getInt(key: String): Int = try { qs[key]?.toInt() } catch(e: NumberFormatException) { null } ?: 2
     val m = getInt("m")
     val n = getInt("n")
 
