a26b4fe99145507748dbfc127e50b342f9ca35f3
Tobin Yehle <tobinyehle@gmail.com>
2016-01-24 00:15:45 -0700

    fix small typo

--- src/Generator.kt
+++ src/Generator.kt
@@ -44,7 +44,7 @@ fun <T> Board<T?>.without(pos: Pos): Board<T?> =
 fun randomBoard(m: Int, n: Int): Board<Int> {
     val emptyBoard = Board<Int?>({i, j -> null}, m*n, m*n)
 //    val threshold = (m*m*n*n) / 3
-    val threshold = Math.pow((m*n).±toDouble(), 1.5)
+    val threshold = Math.pow((m*n).toDouble(), 1.5)
 
     tailrec fun buildBoard(board: Board<Int?> = emptyBoard, tries: Int = 0):Board<Int> =
         if(board.count { it != null } >= threshold) {
